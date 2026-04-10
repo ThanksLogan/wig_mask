ab5f7eb9a3337a4dbd028484fd273ef02e2363e3
Logan <89110766+ThanksLogan@users.noreply.github.com>
2026-04-09 18:29:40 -0700

    Delete wig_mask directory

--- wig_mask/selfie_processing/__init__.py
+++
@@ -1,8 +0,0 @@
-from .pipeline import generate_wig_region_mask
-from .types import WigMaskConfig, WigMaskDebug
-
-__all__ = [
-    "generate_wig_region_mask",
-    "WigMaskConfig",
-    "WigMaskDebug",
-]
